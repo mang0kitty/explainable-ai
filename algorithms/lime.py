e37db84e49f7f0341403380b473c14f792e0a579
Stavros Theocharis <55717550+stavrostheocharis@users.noreply.github.com>
2022-10-25 17:38:08 +0300

    3 deep research on lime (#10)
    * Added Lime docs
    
    * Added text for Lime in Readme
    
    * Lime additions
    
    * Added Lime notebook for numeric values
    
    * Added requirements and adjustements
    
    * Imported no warnings for notebook (lime)
    
    * Fixed output of lime function

---
+++ algorithms/lime.py
@@ -0,0 +1,32 @@
+import lime
+from lime import lime_tabular
+
+
+def create_lime_text_explanation(
+    train_data,
+    model,
+    feature_names,
+    class_names,
+    data_point,
+    num_features=2,
+    top_labels=1,
+    discretize_continuous=True,
+):
+    """
+    If show_all=False only the features used in the explanation are displayed.
+    If discretize_continuous=True, LIME discretizes the features in the explanation. Discretized characteristics provide for better understandable explanations.
+    If show_table=True, a table with features and values is also displayed
+    """
+    explainer = lime_tabular.LimeTabularExplainer(
+        train_data,
+        feature_names=feature_names,
+        class_names=class_names,
+        discretize_continuous=discretize_continuous,
+    )
+    explanation = explainer.explain_instance(
+        data_point,
+        model.predict_proba,
+        num_features=num_features,
+        top_labels=top_labels,
+    )
+    return explanation
